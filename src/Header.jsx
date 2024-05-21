5065c2c41a18276f0a6353742f829be7492402b1
erosalpe <eros.alpegiani@gmail.com>
2024-05-21 14:18:45 +0200

    feat aggiunto bootstrap

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,18 @@
 function Header(){
     return(
         <header>
-            <h1>
-                Test Componente Header React
-            </h1>
+            <nav class="d-flex justify-content-between">
+                <div>
+                    <h1>
+                        Test Componente Header React Sinistra
+                    </h1>
+                </div>
+                <div>
+                    <h1>
+                        Test Componente Header React Destra
+                    </h1>
+                </div>
+            </nav>
         </header>
     )
 }
