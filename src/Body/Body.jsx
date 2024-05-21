049383377ed00d2b25bf586d2e3992f5ba61f29c
erosalpe <eros.alpegiani@gmail.com>
2024-05-21 15:25:38 +0200

    feat aggiunto componente card e popolato dinamicamente utilizzando un file JSON contenente i dati e visualizzato numero di card adeguato al numero di dati

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -1,14 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import './body.css';
-import Card from '../Card/Card.jsx'
-function Body(){
-    return(
-        <div>
+import Card from '../Card/Card.jsx';
+
+function Body() {
+    const [usersData, setUsersData] = useState([]);
+
+    useEffect(() => {
+        fetch('/data.json')
+            .then(response => response.json())
+            .then(data => {
+                setUsersData(data);
+            })
+            .catch(error => console.error('Error loading JSON:', error));
+    }, []);
+
+    return (
+        <div className="container py-5">
             <h4>Il Nostro Team:</h4>
-            <div>
-                <Card/>
+            <div className="d-flex flex-wrap gap-4">
+                {usersData.map((element) => (
+                    <Card
+                        key={element.id}
+                        id={element.id}
+                        name={element.name}
+                        job={element.job}
+                        image={element.image}
+                    />
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default Body
+export default Body;
--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,7 +1,15 @@
-function Card(){
+import './card.css';
+function Card(props){
     return(
-        
-    )
+        <div className="card text-center">
+            <img src={props.image} className="card-img-top rounded-circle w-50 align-self-center mt-3" alt="profile-picture"></img>
+            <div className="card-body">
+                <h5 className="card-title">{props.name}</h5>
+                <p className="card-text">{props.job}</p>
+                <a href="#" className="btn btn-primary">Contattami !</a>
+            </div>
+        </div>
+    );
 }
 
 export default Card
