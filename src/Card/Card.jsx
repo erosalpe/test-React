a9cd4aacbc1dd39ea24936599252a7e710472d40
erosalpe <eros.alpegiani@gmail.com>
2024-05-27 16:55:54 +0200

    feat (card) aggiunto contatore punteggio per testare useState()

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,12 +1,32 @@
 import './card.css';
 import ConfettiButton from '../Button/ConfettiButton.jsx';
+import { useState } from 'react';
 function Card(props){
+
+    const [points, setPoints] = useState(0);
+
+    const incrementPoints = () =>{
+        setPoints(points + 1);
+    }
+
+    const decrementPoints = () =>{
+        if(points > 0){
+            setPoints(points - 1);
+        }
+    }
+
     return(
         <div className="card text-center">
             <img src={props.image} className="card-img-top rounded-circle w-50 align-self-center mt-3" alt="profile-picture"></img>
             <div className="card-body">
                 <h5 className="card-title">{props.name}</h5>
                 <p className="card-text">{props.job}</p>
+                <p>Punti totali: {points}</p>
+                <div className="d-flex align-items-center justify-content-center gap-3 pb-3">
+                    <button onClick={() => incrementPoints()} className="btn btn-success px-3 fs-4">+</button>
+                    <button onClick={() => decrementPoints()} className="btn btn-danger px-3 fs-4">-</button>
+                </div>
+                    
                 <ConfettiButton/>
             </div>
         </div>
